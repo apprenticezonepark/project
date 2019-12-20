30c9896ca343d97d2d9d534415403c74686adbbc
mark_anuchit <numbergreen15@gmail.com>
2019-12-20 15:50:58 +0700

    update home

--- project/src/app/app-routing.module.ts
+++ project/src/app/app-routing.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ApplayoutComponent } from './applayout/applayout.component';
+import { AppComponent } from './app.component';
+
 
 const routes: Routes = [
   {
-    path: 'example',
-    component: ApplayoutComponent
+    path:'',
+    children : [
+      {
+        path:'home',
+        component: ApplayoutComponent
+      }
+    ]
   }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
+  declarations: [],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- project/src/app/app.component.ts
+++ project/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'project';
+  id_bf: string;
+  data_test : any = ['test1','test2','test3','test4','test5']
+
+  ngOnInit() {
+    this.id_bf = "1";
+    document.getElementById(this.id_bf).classList.add("active");
+  }
+
+  active_menu(id) {
+    if (id != null) {
+      document.getElementById(id).classList.add("active");
+      if (id != this.id_bf) {
+        document.getElementById(this.id_bf).classList.remove("active");
+        this.id_bf = id;
+      }
+    } else if (id == null) {
+      var x = document.getElementById("myTopnav");
+      if (x.className === "topnav") {
+        x.className += " responsive";
+      } else {
+        x.className = "topnav";
+      }
+    }
+  }
+
+
 }
