2e54cf5caf4861ce56bcbb0d9cacbcdee253e082
mark_anuchit <numbergreen15@gmail.com>
2019-12-24 10:54:36 +0700

    update appcomponent

--- project/src/app/app-routing.module.ts
+++ project/src/app/app-routing.module.ts
@@ -1,18 +1,23 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { ApplayoutComponent } from './applayout/applayout.component';
-import { AppComponent } from './app.component';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { ApplayoutComponent } from "./applayout/applayout.component";
+import { AppComponent } from "./app.component";
+import { FilenotfoundComponent } from './filenotfound/filenotfound.component';
 
 
 const routes: Routes = [
   {
-    path:'',
-    children : [
+    path: "",
+    children: [
       {
-        path:'home',
+        path: "",
         component: ApplayoutComponent
       }
     ]
+  },
+  {
+    path: "**",
+    component: FilenotfoundComponent
   }
 ];
 
@@ -21,4 +26,4 @@ const routes: Routes = [
   declarations: [],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- project/src/app/app.component.ts
+++ project/src/app/app.component.ts
@@ -1,19 +1,64 @@
-import { Component } from '@angular/core';
-
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'project';
-  id_bf: string;
-  data_test : any = ['test1','test2','test3','test4','test5']
+  title = "project";
+  id_bf: string ;
+  data_test: any = ["test1", "test2", "test3", "test4", "test5"];
+  data_menubar: any = [
+    "หน้าหลัก",
+    "เกษตรอินทรีย์ และอาหารปลอดภัย",
+    "ผลิตผลทางการเกษตร",
+    "พันธุ์พืช",
+    "ผลิตภัณฑ์ชุมชน",
+    "บริการด้านการเกษตร",
+    "ชุมชนต้องเที่ยว",
+    "สินค้าตามภูมิภาค",
+    "ของดี 77 จังหวัด"
+  ];
+
+  data_te: any = [
+    {
+      name: "หน้าหลัก",
+      list: [
+          {
+            namelist : 'test1',
+            test: ["1", "2", "3"]
+          },
+          {
+            namelist : 'test2',
+            test: ["1", "2", "3"],
+          },
+          {
+            namelist : 'test3',
+            test: ["1", "2", "3"]
+          }
+        
+      ]
+    },
+    {
+      name: "เกษตรอินทรีย์ และอาหารปลอดภัย",
+      list: [
+        {
+          test1: ["1", "2", "3"],
+          test2: ["1", "2", "3"],
+          test3: ["1", "2", "3"]
+        }
+      ]
+    }
+  ];
 
   ngOnInit() {
-    this.id_bf = "1";
-    document.getElementById(this.id_bf).classList.add("active");
+    console.log(this.data_te);
+    this.id_bf='1';
+      document.getElementById(this.id_bf).classList.add("active");
+
+    
+
   }
 
   active_menu(id) {
@@ -23,7 +68,8 @@ export class AppComponent {
         document.getElementById(this.id_bf).classList.remove("active");
         this.id_bf = id;
       }
-    } else if (id == null) {
+    } 
+    else if (id == null) {
       var x = document.getElementById("myTopnav");
       if (x.className === "topnav") {
         x.className += " responsive";
@@ -32,6 +78,4 @@ export class AppComponent {
       }
     }
   }
-
-
 }
