0f029c8099335ceaa03202f63c5c2e87e975bb69
mark_anuchit <numbergreen15@gmail.com>
2019-12-24 11:08:03 +0700

    update filenotfound

--- project/src/app/filenotfound/filenotfound.component.ts
+++ project/src/app/filenotfound/filenotfound.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-filenotfound',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FilenotfoundComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
+
+  pagehome() {
+    this.router.navigate([""]);
+  }
+
 
 }
